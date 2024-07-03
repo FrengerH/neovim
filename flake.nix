a30f075ed389f7e14d32f459ac962d3910cb83c7
FrengerH <frengerh@hfmail.nl>
2024-07-03 19:17:40 +0200

    update 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Neovim config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs }:    
